f4f583bb742c51d8ac5c8def9033b6950fbe926d
Attila Nagy <attila@attilanagy.net>
2023-02-23 19:55:07 +0100

    Add components and more unit tests

---
+++ src/components/Criterion.js
@@ -0,0 +1,28 @@
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+const Criterion = ({question, answer, reason, yesHandler, noHandler}) => {
+    return (
+        <Col>
+            <Card text="white" bg={answer !== null ? answer ? 'success' : 'danger' : 'secondary'}>
+                <Card.Body>
+                    <Card.Text>{answer === null ? question : reason}</Card.Text>
+                    {answer === null ?
+                    <>
+                        <Button variant="success" onClick={yesHandler}>Yes</Button>&nbsp;
+                        <Button variant="danger" onClick={noHandler}>No</Button>
+                    </>
+                    :
+                    <>
+                        <Button variant="dark">Yes</Button>&nbsp;
+                        <Button variant="dark">No</Button>
+                    </>
+                    }
+                </Card.Body>
+            </Card>
+        </Col>
+    )
+}
+
+export default Criterion
---
+++ src/components/Criterion.test.js
@@ -0,0 +1,87 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import Criterion from "./Criterion";
+
+describe('unanswered question', () => {
+    let myYesHandler;
+    let myNoHandler;
+
+    beforeEach(() => {
+        myYesHandler = jest.fn();
+        myNoHandler = jest.fn();
+
+        render(<Criterion
+            question={"myQuestion"}
+            answer={null}
+            reason={"myReason"}
+            yesHandler={myYesHandler}
+            noHandler={myNoHandler} />);
+    });
+
+    test('renders body properly', () => {
+        expect(screen.getByText('myQuestion')).toBeInTheDocument()
+    });
+
+    test('yesHandler is called', () => {
+        fireEvent.click(screen.getByText('Yes'));
+        expect(myYesHandler).toBeCalled();
+    });
+
+    test('noHandler is called', () => {
+       fireEvent.click(screen.getByText('No'));
+       expect(myNoHandler).toBeCalled();
+    });
+
+    test('has proper background', () => {
+        const cardElement = screen.getByText('myQuestion').parentElement.parentElement;
+        expect(cardElement).toHaveClass('bg-secondary');
+    });
+
+    test('yes button has proper class', () => {
+        expect(screen.getByText('Yes')).toHaveClass('btn-success');
+    });
+
+    test('no button has proper class', () => {
+        expect(screen.getByText('No')).toHaveClass('btn-danger');
+    });
+});
+
+[
+    {name: 'correct answer', answer: true, expectedClass: 'bg-success'},
+    {name: 'incorrect answer', answer: false, expectedClass: 'bg-danger'}
+].forEach(testCase => {
+    describe('answered question - ' + testCase.name, () => {
+        beforeEach(() => {
+            render(<Criterion
+                question={"myQuestion"}
+                answer={testCase.answer}
+                reason={"myReason"}
+                yesHandler={jest.fn()}
+                noHandler={jest.fn()} />);
+        });
+    
+        test('renders body properly', () => {
+            expect(screen.getByText('myReason')).toBeInTheDocument()
+        });
+
+        test('has proper background', () => {
+            const cardElement = screen.getByText('myReason').parentElement.parentElement;
+            expect(cardElement).toHaveClass(testCase.expectedClass);
+        });
+
+        test('yes button has proper class', () => {
+            expect(screen.getByText('Yes')).toHaveClass('btn-dark');
+        });
+    
+        test('no button has proper class', () => {
+            expect(screen.getByText('No')).toHaveClass('btn-dark');
+        });
+
+        test('yes button has no onClick handler defined', () => {
+            expect(screen.getByText('Yes').onclick).toBe(null);
+        });
+
+        test('no button has no onClick handler defined', () => {
+            expect(screen.getByText('No').onclick).toBe(null);
+        });
+    });
+});
